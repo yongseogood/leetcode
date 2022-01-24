1656c5c0cc7dbddd32cfa1bcadfdb7a3cbaa589c
Yong Seo <yseo@velodyne.com>
2022-01-23 20:23:50 -0800

    Adding 520

---
+++ 520_detect_capital/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        
+        bool allCap {true};
+        for(int i = 0; i < word.size(); ++i)
+        {
+            if(islower(word[i]))
+                allCap = false;
+        }
+        
+        if(allCap) return true;
+
+        for(int i = 1; i < word.size(); ++i)
+        {
+            if((isupper(word[0]) && isupper(word[i])) || (islower(word[0]) && isupper(word[i])))
+                return false;
+        }
+        
+        return true;
+    }
+};
