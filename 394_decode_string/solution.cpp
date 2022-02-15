fa257e12a8aeaaf2e24fc4bb9b8a322e084691ab
Yong Won Seo <yongseogood@gmail.com>
2022-02-15 10:13:18 -0800

    Adding 394

---
+++ 394_decode_string/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        int pos = 0;
+        return helper(pos, s);
+    }
+private:
+    string helper(int& pos, string s) {
+        int num = 0;
+        string word = "";
+        
+        for(; pos < s.size(); ++pos) {
+            char cur = s[pos];
+            if(cur == '[') {
+                string curStr = helper(++pos, s);
+                for(; num > 0; --num) word += curStr;
+            } else if (cur >= '0' && cur <= '9') {
+                num = num * 10 + cur - '0';
+            } else if (cur == ']') {
+                return word;
+            } else {
+                word += cur;
+            }
+        }
+        
+        return word;
+    }
+};
