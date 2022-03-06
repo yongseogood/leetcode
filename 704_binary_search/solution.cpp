4383462d1437b5411104fa231b16d433d3b60260
Yong Won Seo <yongseogood@gmail.com>
2022-03-05 16:49:25 -0800

    Adding 704

---
+++ 704_binary_search/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int h = nums.size() - 1;
+        int l = 0;
+        
+        while(l <= h) {
+            int mid = (h + l) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) h = mid - 1;
+            else l = mid + 1;
+        }
+        
+        return -1;
+    }
+};
