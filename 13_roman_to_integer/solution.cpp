db4037960f81c301e4968e54d20ab364569bccdc
Yong Seo <yongseogood@gmail.com>
2022-03-17 09:45:51 -0700

    Adding 13

---
+++ 13_roman_to_integer/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> map = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        int res = 0;
+        for(int i = 0; i < s.length() - 1; ++i) {
+            if(map[s[i]] < map[s[i + 1]]) res -= map[s[i]];
+            else res += map[s[i]];
+        }
+        
+        return res += map[s.back()];
+    }
+};
