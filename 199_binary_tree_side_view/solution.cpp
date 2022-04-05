6520c303eaaeb586c25eb678c63973b5f797ea7c
Yong Won Seo <yongseogood@gmail.com>
2022-04-05 10:15:06 -0700

    Adding 199

---
+++ 199_binary_tree_side_view/solution.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        helper(root, 1, res);
+        return res;
+    }
+private:
+    void helper(TreeNode* root, int level, vector<int>& res) {
+        if(root == nullptr) return;
+        if(res.size() < level) res.push_back(root->val);
+        helper(root->right, level + 1, res);
+        helper(root->left, level + 1, res);
+    }
+};
