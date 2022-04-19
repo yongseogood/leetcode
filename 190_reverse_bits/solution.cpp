103e75d0e29f1063de3b5212897b2497c3ceea00
Yong Won Seo <yongseogood@gmail.com>
2022-04-19 10:06:44 -0700

    Adding 190

---
+++ 190_reverse_bits/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        bitset<32> bs{n};
+        
+        int l = 0, r = 31;
+        
+        while(l < r) {
+            bool tmp = bs[l];
+            bs[l++] = bs[r];
+            bs[r--] = tmp;
+        }
+        
+        return bs.to_ulong();
+    }
+};
