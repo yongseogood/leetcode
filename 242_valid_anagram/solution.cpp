bf71d34804be607018f5e1f3a7fd2b62b84b04b9
Yong Seo <yongseogood@gmail.com>
2022-08-10 14:34:08 -0700

    adding 242

---
+++ 242_valid_anagram/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        
+        unordered_map<char, int> hash;
+        
+        for(char c : s) hash[c]++;
+        for(char c : t)
+        {
+            hash[c]--;
+            if(hash[c] < 0) return false;
+        }
+        
+        return true;
+    }
+};
