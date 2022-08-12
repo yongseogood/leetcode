0eec65f0293dd15de195d31c483b889ef0b42c70
Yong Seo <yongseogood@gmail.com>
2022-08-12 16:16:35 -0700

    Adding 252

---
+++ 252_meeting_rooms/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        
+        for(int i = 1, n = intervals.size(); i < n; ++i)
+        {
+            if(intervals[i][0] < intervals[i-1][1]) return false;
+        }
+        
+        return true;
+    }
+};
